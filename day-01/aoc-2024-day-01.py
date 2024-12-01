6644d30f884000fcc73ca6c45d3cb1de5a3edf00
Henrik Samuelsson <henrik.samuelsson@gmail.com>
2024-12-01 09:01:44 +0100

    Add code for part 2 and remove debug prints.

--- day-01/aoc-2024-day-01.py
+++ day-01/aoc-2024-day-01.py
@@ -5,25 +5,23 @@ file_path = path.relpath("day-01/data/puzzle-input-aoc-2024-day-01.txt")
 
 with open(file_path) as input_file:
     location_ids = input_file.readlines()
-print(location_ids)
 
 split_location_ids = [loc_id.split() for loc_id in location_ids]
-print(split_location_ids)
-
 left_ids = [int(loc_id[0]) for loc_id in split_location_ids]
-print(left_ids)
-
 right_ids = [int(loc_id[1]) for loc_id in split_location_ids]
-print(right_ids)
-
 left_ids.sort()
-print(left_ids)
-
 right_ids.sort()
-print(right_ids)
-
 distances = [abs(left - right) for left, right in zip(left_ids, right_ids)]
-print(distances)
-
 total_distance = sum(distances)
-print(total_distance)
+
+similarity_score = []
+for right_id in right_ids:
+    match_count = 0
+    for left_id in left_ids:
+        if right_id == left_id:
+            match_count += 1
+    similarity_score.append(right_id * match_count)
+total_similarity_score = sum(similarity_score)
+
+print("Answer part 1:", total_distance)
+print("Answer part 2:", total_similarity_score)
