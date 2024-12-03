cc14bf67186fb673e1bb2189abea86427897c02c
Henrik Samuelsson <henrik.samuelsson@gmail.com>
2024-12-03 19:49:29 +0100

    Messy code but in kind of works.

--- day-03/aoc-2024-day-03.py
+++ day-03/aoc-2024-day-03.py
@@ -5,18 +5,51 @@ import re
 file_path = path.relpath("day-03/data/puzzle-input-aoc-2024-day-03.txt")
 
 with open(file_path) as input_file:
-    input_lines = input_file.read().splitlines()
+    input_lines = input_file.read()
+    line = input_lines.replace('\n', '')
+    print(input_lines)
+
+pattern = r'mul\(\d{1,3},\d{1,3}\)'
 
 answer_part_1 = 0
 
-for line in input_lines:
-    print(line)
+filtered_words = re.findall(pattern, line)
+#print(filtered_words)
 
-    pattern = r'mul\(\d{1,3},\d{1,3}\)'
-    # Find all occurrences that match the pattern
-    filtered_words = re.findall(pattern, line)
+for word in filtered_words:
+    parts = word.split(',')
+    # print(parts)
+    number1 = int(re.sub(r'\D', '', parts[0]))
+    # print(number1)
+    number2 = int(re.sub(r'\D', '', parts[1]))
+    # print(number2)
+    answer_part_1 += number1 * number2
+
+answer_part_2 = 0
+
+
+# Start with the header part before the first don't() instruction.
+header = line.split("don't()")[0]
+print("header", header)
+
+filtered_words = re.findall(pattern, header)
+print(filtered_words)
+
+for word in filtered_words:
+    parts = word.split(',')
+    #print(parts)
+    number1 = int(re.sub(r'\D', '', parts[0]))
+    #print(number1)
+    number2 = int(re.sub(r'\D', '', parts[1]))
+    #print(number2)
+    answer_part_2 += number1 * number2
+
+result = re.findall("do\(\)(.*?)don't\(\)", line)
+print("result", result)
+
+for s in result:
+    filtered_words = re.findall(pattern, s)
     print(filtered_words)
-
 
     for word in filtered_words:
         parts = word.split(',')
@@ -25,7 +58,7 @@ for line in input_lines:
         print(number1)
         number2 = int(re.sub(r'\D', '', parts[1]))
         print(number2)
-        answer_part_1 += number1 * number2
+        answer_part_2 += number1 * number2
 
 print("Answer part 1:", answer_part_1)
-print("Answer part 2:", 0)
+print("Answer part 2:", answer_part_2)
