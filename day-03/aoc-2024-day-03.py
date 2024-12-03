48de9dce7f45fd32244c65bc173b90e03316993e
Henrik Samuelsson <henrik.samuelsson2@saabgroup.com>
2024-12-03 07:04:19 +0100

    Add first version of code solving part 1, day 3 2024.

--- day-03/aoc-2024-day-03.py
+++ day-03/aoc-2024-day-03.py
@@ -1,10 +1,31 @@
 from os import path
+import re
 
 # Relative path to the file holding input data.
-file_path = path.relpath("day-02/data/puzzle-input-aoc-2024-day-02.txt")
+file_path = path.relpath("day-03/data/puzzle-input-aoc-2024-day-03.txt")
 
 with open(file_path) as input_file:
     input_lines = input_file.read().splitlines()
 
-print("Answer part 1:", 0)
+answer_part_1 = 0
+
+for line in input_lines:
+    print(line)
+
+    pattern = r'mul\(\d{1,3},\d{1,3}\)'
+    # Find all occurrences that match the pattern
+    filtered_words = re.findall(pattern, line)
+    print(filtered_words)
+
+
+    for word in filtered_words:
+        parts = word.split(',')
+        print(parts)
+        number1 = int(re.sub(r'\D', '', parts[0]))
+        print(number1)
+        number2 = int(re.sub(r'\D', '', parts[1]))
+        print(number2)
+        answer_part_1 += number1 * number2
+
+print("Answer part 1:", answer_part_1)
 print("Answer part 2:", 0)
