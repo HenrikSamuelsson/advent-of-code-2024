22d7532e10abaf2f9d72c1dae489b17921c12584
Henrik Samuelsson <henrik.samuelsson@gmail.com>
2024-12-05 19:40:51 +0100

    Add start of day 5.

--- day-05/aoc-2024-day-05.py
+++ day-05/aoc-2024-day-05.py
@@ -4,7 +4,15 @@ from os import path
 file_path = path.relpath("day-05/data/test-input.txt")
 
 with open(file_path) as input_file:
-    input_lines = input_file.read().splitlines()
+    input_lines = input_file.read().split('\n\n')
+
+print(input_lines)
+
+rules = input_lines[0].split()
+updates = input_lines[1].split()
+
+print(rules)
+print(updates)
 
 print("Answer part 1:", 0)
 print("Answer part 2:", 0)
