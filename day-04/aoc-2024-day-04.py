021cffbe968e685f122776072bd2e1ba6d691e5c
Henrik Samuelsson <henrik.samuelsson2@saabgroup.com>
2024-12-05 12:58:13 +0100

    Add checks for three more directions.

--- day-04/aoc-2024-day-04.py
+++ day-04/aoc-2024-day-04.py
@@ -4,7 +4,7 @@ from os import path
 def word_at_north(word, input_lines, line_count, col_count):
     # Start by checking if word will even fit to the north.
     if word_length > line_count + 1:  # Add 1 because line count starts at 0.
-        # Word is too long to fit horizontally
+        # Word is too long to fit at north.
         return False
     # Check if all characters in the word are present.
     for idx, c in enumerate(word):
@@ -18,7 +18,7 @@ def word_at_north(word, input_lines, line_count, col_count):
 def word_at_south(word, input_lines, line_count, col_count):
     # Start by checking if word will even fit to the south.
     if word_length > len(input_lines) - line_count:
-        # Word is too long to fit horizontally
+        # Word is too long to fit at south.
         return False
     # Check if all characters in the word are present.
     for idx, c in enumerate(word):
@@ -32,7 +32,7 @@ def word_at_south(word, input_lines, line_count, col_count):
 def word_at_west(word, input_lines, line_count, col_count):
     # Start by checking if word will even fit to the west.
     if word_length > col_count + 1:
-        # Word is too long to fit horizontally
+        # Word is too long to fit to the west.
         return False
     # Check if all characters in the word are present.
     for idx, c in enumerate(word):
@@ -40,6 +40,56 @@ def word_at_west(word, input_lines, line_count, col_count):
         if c != line[col_count - idx]:
             return False
     print("Word at west from line", line_count, "column", col_count)
+    return True
+
+
+def word_at_east(word, input_lines, line_count, col_count):
+    # Start by checking if word will even fit to the east.
+    if word_length > len(input_lines[0]) - col_count:
+        # Word is too long to fit to the east.
+        return False
+    # Check if all characters in the word are present.
+    for idx, c in enumerate(word):
+        line = input_lines[line_count]
+        if c != line[col_count + idx]:
+            return False
+    print("Word at east from line", line_count, "column", col_count)
+    return True
+
+
+def word_at_north_east(word, input_lines, line_count, col_count):
+    # Start by checking if word will even fit to the north.
+    if word_length > line_count + 1:  # Add 1 because line count starts at 0.
+        # Word is too long to fit at north.
+        return False
+    # Then check if word will even fit to the east.
+    if word_length > len(input_lines[0]) - col_count:
+        # Word is too long to fit to the east.
+        return False
+    # Check if all characters in the word are present.
+    for idx, c in enumerate(word):
+        line = input_lines[line_count - idx]
+        if c != line[col_count + idx]:
+            return False
+    print("Word at north east from line", line_count, "column", col_count)
+    return True
+
+
+def word_at_north_west(word, input_lines, line_count, col_count):
+    # Start by checking if word will even fit to the north.
+    if word_length > line_count + 1:  # Add 1 because line count starts at 0.
+        # Word is too long to fit at north.
+        return False
+    # Then check if word will even fit to the east.
+    if word_length > col_count + 1:
+        # Word is too long to fit to the east.
+        return False
+    # Check if all characters in the word are present.
+    for idx, c in enumerate(word):
+        line = input_lines[line_count - idx]
+        if c != line[col_count - idx]:
+            return False
+    print("Word at north west from line", line_count, "column", col_count)
     return True
 
 
@@ -70,6 +120,12 @@ for line in input_lines:
         if word_at_south(word, input_lines, line_count, col_count):
             word_count += 1
         if word_at_west(word, input_lines, line_count, col_count):
+            word_count += 1
+        if word_at_east(word, input_lines, line_count, col_count):
+            word_count += 1
+        if word_at_north_east(word, input_lines, line_count, col_count):
+            word_count += 1
+        if word_at_north_west(word, input_lines, line_count, col_count):
             word_count += 1
         col_count += 1
     line_count += 1
