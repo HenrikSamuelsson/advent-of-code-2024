897e40b521793d620fddde5c15b7d09a2f06072d
Henrik Samuelsson <henrik.samuelsson2@saabgroup.com>
2024-12-06 07:17:38 +0100

    Some more work an day 5 part 1.

--- day-05/aoc-2024-day-05.py
+++ day-05/aoc-2024-day-05.py
@@ -6,13 +6,29 @@ file_path = path.relpath("day-05/data/test-input.txt")
 with open(file_path) as input_file:
     input_lines = input_file.read().split('\n\n')
 
-print(input_lines)
-
 rules = input_lines[0].split()
 updates = input_lines[1].split()
 
-print(rules)
-print(updates)
+
+
+for update in updates:
+    pages = update.split(',')
+    update_in_correct_order = True
+    for idx, current_page in enumerate(pages):
+        # Check rules for pages before current page.
+        print("pages before current page")
+        for other_page_idx in range(0, idx):
+            print(current_page, pages[other_page_idx])
+        # Check rules far pages after current page.
+        print("pages after current page")
+        for other_page_idx in range(idx + 1, len(pages)):
+            print(current_page, pages[other_page_idx])
+        for rule in rules:
+            split_rule = rule.split('|')
+            #print(split_rule)
+            #if page == split_rule[1]:
+            #    for page in pages:
+
 
 print("Answer part 1:", 0)
 print("Answer part 2:", 0)
