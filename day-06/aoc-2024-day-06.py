0764c2ca1b0b8fee45704f29af0ae20f84f029f9
Henrik Samuelsson <henrik.samuelsson@gmail.com>
2024-12-06 20:40:01 +0100

    Add start of day 7.

--- day-06/aoc-2024-day-06.py
+++ day-06/aoc-2024-day-06.py
@@ -1,10 +1,54 @@
+from dataclasses import dataclass
+from enum import Enum
 from os import path
 
+
+@dataclass
+class Point:
+    x: int
+    y: int
+
+
+class Direction(Enum):
+    NORTH = "north"
+    EAST = "east"
+    SOUTH = "south"
+    WEST = "west"
+
+
+def next_direction(current_direction):
+    if current_direction == Direction.NORTH:
+        return Direction.EAST
+    if current_direction == Direction.EAST:
+        return Direction.SOUTH
+    if current_direction == Direction.SOUTH:
+        return Direction.WEST
+    if current_direction == Direction.WEST:
+        return Direction.NORTH
+
+
 # Relative path to the file holding input data.
-file_path = path.relpath("day-06/data/test-input.txt")
+file_path = path.relpath("day-07/data/test-input.txt")
 
 with open(file_path) as input_file:
     input_lines = input_file.read().splitlines()
+
+y_size = len(input_lines) - 1
+x_size = len(input_lines[0]) - 1
+
+visited_locations = [['.']*x_size for _ in range(y_size)]
+
+start = Point(0, 0)
+direction = Direction.NORTH
+
+for y_idx, line in enumerate(input_lines):
+    for x_idx, c in enumerate(line):
+        if c == '^':
+            start.x = x_idx
+            start.y = y_idx
+            visited_locations[y_idx][x_idx] = 'X'
+        if c == "#":
+            visited_locations[y_idx][x_idx] = '#'
 
 print("Answer part 1:", 0)
 print("Answer part 2:", 0)
