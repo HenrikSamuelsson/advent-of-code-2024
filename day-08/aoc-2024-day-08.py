64473ce3a9fdac7cb2cb8ef2cef939092e56e1d5
Henrik Samuelsson <henrik.samuelsson2@saabgroup.com>
2024-12-10 07:01:15 +0100

    Store the antennas in a map.

--- day-08/aoc-2024-day-08.py
+++ day-08/aoc-2024-day-08.py
@@ -4,7 +4,17 @@ from os import path
 file_path = path.relpath("day-08/data/test-input-1.txt")
 
 with open(file_path) as input_file:
-    input_lines = input_file.read().splitlines()
+    input_rows = input_file.read().splitlines()
+
+antenna_map = {}
+
+num_of_rows = len(input_rows)
+num_of_cols = len(input_rows[0])
+
+for row_idx, row in enumerate(input_rows):
+    for col_idx, value, in enumerate(row):
+        if value != '.':
+            antenna_map[(row_idx, col_idx)] = value
 
 print("Answer part 1:", 0)
 print("Answer part 2:", 0)
