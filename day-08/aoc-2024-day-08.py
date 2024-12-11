8cc0ee058b1ee5550aed37a689b6b0c3b0fe7b50
Henrik Samuelsson <henrik.samuelsson2@saabgroup.com>
2024-12-11 06:38:14 +0100

    Add dictionary for storing anti nodes.

--- day-08/aoc-2024-day-08.py
+++ day-08/aoc-2024-day-08.py
@@ -7,6 +7,7 @@ with open(file_path) as input_file:
     input_rows = input_file.read().splitlines()
 
 antenna_map = {}
+anti_nodes_map = {}
 
 num_of_rows = len(input_rows)
 num_of_cols = len(input_rows[0])
@@ -15,6 +16,7 @@ for row_idx, row in enumerate(input_rows):
     for col_idx, value, in enumerate(row):
         if value != '.':
             antenna_map[(row_idx, col_idx)] = value
+
 
 print("Answer part 1:", 0)
 print("Answer part 2:", 0)
