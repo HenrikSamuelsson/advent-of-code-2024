196a71d8f4e92d508681f1975273090389fae4bf
Henrik Samuelsson <henrik.samuelsson2@saabgroup.com>
2024-12-12 06:58:20 +0100

    Add code that solves day 8 part 1.

--- day-08/aoc-2024-day-08.py
+++ day-08/aoc-2024-day-08.py
@@ -1,7 +1,7 @@
 from os import path
 
 # Relative path to the file holding input data.
-file_path = path.relpath("day-08/data/test-input-2.txt")
+file_path = path.relpath("day-08/data/puzzle-input.txt")
 with open(file_path) as input_file:
     input_rows = input_file.read().splitlines()
 
@@ -16,8 +16,27 @@ for row_idx, row in enumerate(input_rows):
         if value != '.':
             antenna_map[(row_idx, col_idx)] = value
 
-for antenna in antenna_map:
-    print(antenna[0], antenna[1], antenna_map[antenna])
+for ant_1 in antenna_map:
+    print("ant_1", ant_1[0], ant_1[1], antenna_map[ant_1])
+    for ant_2 in antenna_map:
+        if (ant_1 != ant_2):
+            if (antenna_map[ant_1] == antenna_map[ant_2]):
+                print("ant_2", ant_2[0], ant_2[1], antenna_map[ant_2])
+                delta_y = ant_2[0] - ant_1[0]
+                anti_node_y = ant_2[0] + delta_y
+                delta_x = ant_2[1] - ant_1[1]
+                anti_node_x = ant_2[1] + delta_x
+                anti_node_is_on_map = True
+                if anti_node_x < 0:
+                    anti_node_is_on_map = False
+                if anti_node_y < 0:
+                    anti_node_is_on_map = False
+                if anti_node_x >= num_of_cols:
+                    anti_node_is_on_map = False
+                if anti_node_y >= num_of_rows:
+                    anti_node_is_on_map = False
+                if anti_node_is_on_map:
+                    anti_nodes_map[(anti_node_y, anti_node_x)] = '#'
 
-print("Answer part 1:", 0)
+print("Answer part 1:", len(anti_nodes_map))
 print("Answer part 2:", 0)
